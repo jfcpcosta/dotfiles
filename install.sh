5bb9f2a1709cca70a1f6d5330797e01f5ad67c53
Francisco Costa <jfcpcosta@gmail.com>
2016-12-25 16:39:41 +0000

    add gitconfig to install scripts

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s "$PWD/vim" "$HOME/.vim"
 ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
 ln -s "$PWD/teamocil" "$HOME/.teamocil"
 ln -s "$PWD/gitignore_global" "$HOME/.gitignore_global"
+ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
 ln -s "$PWD/my.cnf" "$HOME/.my.cnf"
 
 gem install teamocil
