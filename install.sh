e53a0a811aa05063bc4544ae1ddfed193679b45c
Francisco Costa <jfcpcosta@gmail.com>
2019-07-31 02:31:03 +0100

    add spaceship to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Spaceship Theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Spaceship Plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+
+
 ln -s "$PWD/zshrc" "$HOME/.zshrc"
 ln -s "$PWD/vimrc" "$HOME/.vimrc"
 ln -s "$PWD/vim" "$HOME/.vim"
