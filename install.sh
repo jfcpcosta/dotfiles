c52ae3a180a395c32b3da9e5bd78eb067eb8f31c
Francisco Costa <jfcpcosta@gmail.com>
2019-08-21 22:04:57 +0100

    fix installation file

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Spaceship Theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
@@ -7,14 +10,17 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 # Spaceship Plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 
+# Create vim plugins folder
+mkdir vim
 
+# Create link to configuration files
 ln -s "$PWD/zshrc" "$HOME/.zshrc"
 ln -s "$PWD/vimrc" "$HOME/.vimrc"
 ln -s "$PWD/vim" "$HOME/.vim"
-ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
-ln -s "$PWD/teamocil" "$HOME/.teamocil"
+#ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
+#ln -s "$PWD/teamocil" "$HOME/.teamocil"
 ln -s "$PWD/gitignore_global" "$HOME/.gitignore_global"
 ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
 ln -s "$PWD/my.cnf" "$HOME/.my.cnf"
 
-gem install teamocil
+#gem install teamocil
