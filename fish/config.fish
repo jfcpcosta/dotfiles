7012d8fe31776c9ea6308be82b8e9f88de0f6aec
Francisco Costa <jfcpcosta@gmail.com>
2024-01-07 14:03:16 +0000

    chore: updates

--- fish/config.fish
+++ fish/config.fish
@@ -9,5 +9,7 @@ starship init fish | source
 set -x N_PREFIX $HOME/.n
 set -x PATH $N_PREFIX/bin:$PATH
 
+set -x PATH ~/Library/Android/sdk/platform-tools:$PATH
+
 # Aliases
 alias cat="bat --theme=$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
