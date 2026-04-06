df9362d9d39d31f04281362c21e583fb800b4161
Francisco Costa <francisco@mothership.local>
2026-04-06 15:33:42 +0100

    chore: add adb to path and add eas to npm

--- config/fish/conf.d/10-path.fish
+++ config/fish/conf.d/10-path.fish
@@ -2,3 +2,9 @@ fish_add_path ~/.local/bin
 fish_add_path ~/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.cargo/bin
+
+# Android SDK
+set -gx ANDROID_HOME $HOME/Library/Android/sdk
+fish_add_path $ANDROID_HOME/platform-tools
+fish_add_path $ANDROID_HOME/emulator
+fish_add_path $ANDROID_HOME/cmdline-tools/latest/bin
