7b49c297b64acf407655ed0bbb1c69d0c99985dd
Francisco Costa <jfcpcosta@gmail.com>
2026-04-10 00:15:52 +0100

    feat: add and configure grc to colorize commands

--- config/fish/conf.d/30-integrations.fish
+++ config/fish/conf.d/30-integrations.fish
@@ -13,3 +13,11 @@ end
 if command -v direnv >/dev/null
     direnv hook fish | source
 end
+
+if command -v grc >/dev/null
+    set -l grc_prefix (brew --prefix grc 2>/dev/null)
+
+    if test -n "$grc_prefix"; and test -f "$grc_prefix/.bottle/etc/grc.fish"
+        source "$grc_prefix/.bottle/etc/grc.fish"
+    end
+end
